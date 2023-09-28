f9d7271a0c2a79ed81017ef6711e6db359445c94
SiverinNikita <nikita.siverin@hubbiton.info>
2023-09-29 01:36:43 +0300

    dynamic html

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 let form = document.querySelector(".main_page_content_form");
-console.log(form);
 let number = form.querySelector(".number_human");
 let fields = form.querySelectorAll(".field");
 
-// number.addEventListener("input", function () {});
+out = `            <div class="hero">
+<input type="text" class="FIO" placeholder="ФИО">
+<input type="text" class="rang" placeholder="Должность">
+<input type="text" class="place" placeholder="Кафедра">
+<input type="email" class="mail" placeholder="Почта">
+<input type="tel" class="phone" placeholder="Номер телефона +7...">
+</div>`
+// присвоить name инпутам!! и валидацию написать !! и ещё php бэк с бд
+number.oninput = function () {
+    document.querySelector('.humans').innerHTML = ''
+    for (let index = 0; index < number.value; index++) {
+        document.querySelector(".humans").insertAdjacentHTML('beforeend', out);
+        console.log(2);
+    }
+};
+
 
 form.addEventListener("submit", function () {
   event.preventDefault();
