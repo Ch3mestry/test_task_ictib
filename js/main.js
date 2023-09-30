3c1ae2c7e23bed287142147b63be7c93fd86bf49
SiverinNikita <nikita.siverin@hubbiton.info>
2023-10-01 02:05:25 +0300

    low validate

--- js/main.js
+++ js/main.js
@@ -1,25 +1,46 @@
 let form = document.querySelector(".main_page_content_form");
 let number = form.querySelector(".number_human");
 let fields = form.querySelectorAll(".field");
+let parentNumber = form.querySelector(".count_human");
 
-// присвоить name инпутам!! и валидацию написать !! и ещё php бэк с бд !!
+let generateError = function (text) {
+  var error = document.createElement("div");
+  error.className = "error";
+  error.style.color = "red";
+  error.innerHTML = text;
+  return error;
+};
+
+function validate(evt) {
+  var theEvent = evt || window.event;
+  var key = theEvent.keyCode || theEvent.which;
+  key = String.fromCharCode(key);
+  var regex = /[1-9\s]|\./;
+  if (!regex.test(key)) {
+    theEvent.returnValue = false;
+    if (theEvent.preventDefault) theEvent.preventDefault();
+  }
+}
+
 number.oninput = function () {
   document.querySelector(".humans").innerHTML = "";
-  for (let index = 0; index < number.value; index++) {
-    document.querySelector(".humans").insertAdjacentHTML(
-      "beforeend",
-      `<div class="hero">
-        <input type="text" name = "fio ${index} field" class="FIO" placeholder="ФИО">
-        <input type="text" name = "jobtitle ${index} field" class="rang" placeholder="Должность">
-        <input type="text" name = "department ${index} field" class="place" placeholder="Кафедра">
-        <input type="email" name = "mail ${index} field" class="mail" placeholder="Почта">
-        <input type="tel" name = "telephone ${index} field" class="phone" placeholder="Номер телефона +7...">
+  if (number.value > 0) {
+    for (let index = 0; index < number.value; index++) {
+      document.querySelector(".humans").insertAdjacentHTML(
+        "beforeend",
+        `<div class="hero">
+        <input type="text" name = "fio ${index}" class="FIO field" placeholder="ФИО">
+        <input type="text" name = "jobtitle ${index}" class="rang field" placeholder="Должность">
+        <input type="text" name = "department ${index}" class="place field" placeholder="Кафедра">
+        <input type="email" name = "mail ${index}" class="mail field" placeholder="Почта">
+        <input type="tel" onkeypress="validate(event)" maxlength='16' name = "telephone ${index}" class="phone field" placeholder="Номер телефона +7...">
         </div>`
-    );
+      );
+    }
   }
 };
 
-form.addEventListener("submit", function () {
+form.addEventListener("button", function () {
   event.preventDefault();
 
   var errors = form.querySelectorAll(".error");
@@ -27,12 +48,14 @@ form.addEventListener("submit", function () {
     errors[er].remove();
   }
 
+  if (!number.value) {
+    var error = generateError("Введите число от 1 до 9");
+    parentNumber.insertAdjacentElement("afterbegin", error);
+  }
+
   for (let index = 0; index < fields.length; index++) {
     if (!fields[index].value) {
-      var error = document.createElement("div");
-      error.className = "error";
-      error.style.color = "red";
-      error.innerHTML = "Поле пустое";
+      var error = generateError("Заполните поле ответа");
       form[index].parentElement.insertBefore(error, fields[index]);
     }
   }
