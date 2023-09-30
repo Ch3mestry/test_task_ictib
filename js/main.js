3b60de706502e6b59addccef252b50126301d119
SiverinNikita <nikita.siverin@hubbiton.info>
2023-10-01 00:41:00 +0300

    last update layot

--- js/main.js
+++ js/main.js
@@ -2,22 +2,22 @@ let form = document.querySelector(".main_page_content_form");
 let number = form.querySelector(".number_human");
 let fields = form.querySelectorAll(".field");
 
-out = `            <div class="hero">
-<input type="text" class="FIO" placeholder="ФИО">
-<input type="text" class="rang" placeholder="Должность">
-<input type="text" class="place" placeholder="Кафедра">
-<input type="email" class="mail" placeholder="Почта">
-<input type="tel" class="phone" placeholder="Номер телефона +7...">
-</div>`
-// присвоить name инпутам!! и валидацию написать !! и ещё php бэк с бд
+// присвоить name инпутам!! и валидацию написать !! и ещё php бэк с бд !!
 number.oninput = function () {
-    document.querySelector('.humans').innerHTML = ''
-    for (let index = 0; index < number.value; index++) {
-        document.querySelector(".humans").insertAdjacentHTML('beforeend', out);
-        console.log(2);
-    }
+  document.querySelector(".humans").innerHTML = "";
+  for (let index = 0; index < number.value; index++) {
+    document.querySelector(".humans").insertAdjacentHTML(
+      "beforeend",
+      `<div class="hero">
+        <input type="text" name = "fio ${index} field" class="FIO" placeholder="ФИО">
+        <input type="text" name = "jobtitle ${index} field" class="rang" placeholder="Должность">
+        <input type="text" name = "department ${index} field" class="place" placeholder="Кафедра">
+        <input type="email" name = "mail ${index} field" class="mail" placeholder="Почта">
+        <input type="tel" name = "telephone ${index} field" class="phone" placeholder="Номер телефона +7...">
+        </div>`
+    );
+  }
 };
-
 
 form.addEventListener("submit", function () {
   event.preventDefault();
